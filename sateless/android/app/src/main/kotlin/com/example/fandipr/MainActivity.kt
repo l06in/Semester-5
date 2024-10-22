29e806113083b6be6fa1ec2ef2b2281f0875e806
l06in <mudin.imam24@gmail.com>
2024-10-22 23:52:08 +0700

    Stateless & Statefull

---
+++ sateless/android/app/src/main/kotlin/com/example/fandipr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fandipr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ sateless/pages/Home.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'pages/Home.dart';
+
+void main()
+  { runApp (MyApp()); }
